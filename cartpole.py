6fcfe23c4373a8ee6956dafaf9299baa695d1cc1
Kundan siwakoti <Siwakotik89@gmail.com>
2025-08-10 22:48:26 +0545

    Removed typo

--- cartpole.py
+++ cartpole.py
@@ -17,23 +17,35 @@ class cartople:
         w_dot = states[2]  # Pole Anuglar velocity
         w = states[3]      # Pole Anuglar position
 
+        # print("Init states:",type(x_dot), type(x), type(w_dot), type(w))
         s_theta = math.sin(w)
         c_theta = math.cos(w)
+        # print("Sin/cos:", s_theta, c_theta)
 
         total_mass = self.cart_mass + self.pole_mass
+        # print("mass:", total_mass)
 
         ''' Calculate w_ddot'''
         inside_bracket = (- force - self.pole_mass * self.pole_half_length * w_dot* w_dot * s_theta) / total_mass
+        # print("Inside bracket:", inside_bracket)
         num = g*s_theta + c_theta * inside_bracket
         deno = self.pole_half_length * ( 4/3 - (self.pole_mass* c_theta * c_theta)/total_mass)
+        # print("Num:", num)
+        # print("Deno:", deno)
 
         w_ddot = num/deno
 
         ''' Calculate x_ddot'''
-        inside_bracket = w_dot*w_dot*s_theta - w_ddot*w_ddot*c_theta
+        inside_bracket = w_dot*w_dot*s_theta - w_ddot*c_theta
         num = force + self.pole_mass * self.pole_half_length * inside_bracket
         deno = total_mass
+        # print("Num:", num)
+        # print("Deno:", deno)
 
         x_ddot = num/deno
+        # print(x_ddot)
+        # print(x_dot)
+        # print(w_ddot)
+        # print(w_dot)
 
         return np.array([x_ddot,x_dot,w_ddot,w_dot],  dtype=float)
