8bea5f3957072b307a3930d9ac948a841e80f556
Kundan siwakoti <Siwakotik89@gmail.com>
2025-08-15 08:12:21 +0545

    Added rulehandler class and parser function

--- fuzzy/fuzzy.py
+++ fuzzy/fuzzy.py
@@ -1,9 +1,67 @@
 from memberships_functions import MembershipFunctionFactory as mfs
+import re
 import math
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 class fuzzy():
+    class ruleHandler:
+        def __init__(self):
+            self.rules: list = []
+            self.parsed_rules : dict = {}
+            self.numberOfRules : int = 0
+            self.antecedentsLVs : dict ={}
+            self.consequentLVs : dict ={}
+            self.fuzzy_operators = ["And", "Or", "Not"]
+
+            
+        def add_rules(self, rule_string:list, antecedentLVs, consequentLVs):
+            self.antecedentsLVs = antecedentLVs
+            self.consequentLVs = consequentLVs
+
+            for r in rule_string:
+                self.rules.append(r)
+            self.numberOfRules = len(self.rules)
+            
+
+        def parse_rule(self):
+            antecedents_dict : dict = {}
+            consequents_dict : dict = {}
+            for i,r in enumerate(self.rules):
+                splited = r.split("Then")
+                antecedents = splited[0].split("If")[-1]
+                consequents = splited[1]     
+
+                keywords_a = re.findall(r"\b(?:is|and|or|not)\b", antecedents, flags=re.IGNORECASE)
+                antecedents_dict = dict(re.findall(r"(\w+)\s+is\s+(\w+)", antecedents))
+
+                keywords_c = re.findall(r"\b(?:is|and|or|not)\b", consequents, flags=re.IGNORECASE)
+                consequents_dict = dict(re.findall(r"(\w+)\s+is\s+(\w+)", consequents))
+
+                self.parsed_rules[f"rule_{i}"] = {
+                    "antecedents_operations": keywords_a,
+                    "antecedents": antecedents_dict,
+                    "consequents_operations": keywords_c,
+                    "consequents": consequents_dict
+                }
+        def fuzzy_or(self, x, y):
+            return max(x,y)
+        
+        def fuzzy_and(self, x, y):
+            return min(x,y)
+        
+        def fuzzy_not(self, x):
+            return (1.0-x)
+
+
+        def rule_inference(self):
+            pass
+
+
+
+
+
     class memFunctions:
         def __init__(self, name=" ", params=None , type=None):
             self.name = name
@@ -38,10 +96,10 @@ class fuzzy():
 
     
     class Output:
-        def __init__(self, Name="output", Range=[-1,1], NumMFS=1):
+        def __init__(self, Name="output", Range=[-1,1], NumMFs=1):
             self.name = Name
             self.range = Range
-            self.nummfs = NumMFS
+            self.nummfs = NumMFs
             self.MembershipFunctions = [fuzzy.memFunctions() for _ in range(self.nummfs) ]
 
             
@@ -50,61 +108,125 @@ class fuzzy():
             self.nummfs += 1
     
 
-    def __init__(self,Name = "",NumInputs=1, NumInputMFs=1,NumOutputs=1,NumOutputMfs=1,rule="AddRule"):
+    def __init__(self,Name = "",NumInputs=1, NumInputMFs=1,NumOutputs=1,NumOutputMFs=1,rule="AddRule"):
         self.name = Name
+
+        self.numIn = NumInputs    
+        self.numInMFS = NumInputMFs
+        self.numOut = NumOutputs    
+        self.numOutMFS = NumOutputMFs
+
         self.input = [self.Input(NumMFs=NumInputMFs) for _ in range(NumInputs)]
-        self.output = [self.Output(NumMFS=NumOutputMfs) for _ in range(NumOutputs)]
+        self.output = [self.Output(NumMFs=NumOutputMFs) for _ in range(NumOutputs)]
 
+        self.antecedentLnguisticVariables : dict = {}
+        self.consequentLnguisticVariables : dict = {}
 
+        self.ruleHndl = self.ruleHandler()
 
+    
+    def update_linguistic_variable(self):
+        for i in self.input:
+            key = i.name
+            value = []
+            for j in range(i.nummfs):
+                value.append(i.MembershipFunctions[j].name)
+            self.antecedentLnguisticVariables[key] = value
 
+        for i in self.output:
+            key = i.name
+            value = []
+            for j in range(i.nummfs):
+                value.append(i.MembershipFunctions[j].name)
+            self.consequentLnguisticVariables[key] = value
+    
+    def add_rule(self, rule):
+        self.update_linguistic_variable()
+        self.ruleHndl.add_rules(rule, self.antecedentLnguisticVariables, self.consequentLnguisticVariables)
+    
+    def compute(self):
+        
+        pass
+
+def test():
+    parsed_rules : dict = {}
+    antecedents_dict : dict = {}
+    consequents_dict : dict = {}
+    rules =["IF vel is h Then force is h","If an is as  And color is read Then spee is more Or cost is high"]
+    for i,r in enumerate(rules):
+                splited = r.split("Then")
+                antecedents = splited[0].split("If")[-1]
+                consequents = splited[1]     
+
+                keywords_a = re.findall(r"\b(?:is|and|or|not)\b", antecedents, flags=re.IGNORECASE)
+                antecedents_dict = dict(re.findall(r"(\w+)\s+is\s+(\w+)", antecedents))
+
+                keywords_c = re.findall(r"\b(?:is|and|or|not)\b", consequents, flags=re.IGNORECASE)
+                consequents_dict = dict(re.findall(r"(\w+)\s+is\s+(\w+)", consequents))
+
+                parsed_rules[f"rule_{i}"] = {
+                    "antecedents_operations": keywords_a,
+                    "antecedents": antecedents_dict,
+                    "consequents_operations": keywords_c,
+                    "consequents": consequents_dict
+                }
+                # print(keywords_a)
+                # print(keywords_c)
+    print(parsed_rules)
+
+                
 if __name__ == "__main__":
-    fis = fuzzy("Cartpole-controller", 1, 2, 1 ,2)
-    fis.input[0].name = "Theta"
-    fis.input[0].range = [-math.pi, math.pi]
-    fis.input[0].MembershipFunctions[0].name = "Negtive"
-    fis.input[0].MembershipFunctions[0].type = "zmf"
-    fis.input[0].MembershipFunctions[0].params = [-0.5, 0.5]
-    fis.input[0].MembershipFunctions[1].name = "Negtive"
-    fis.input[0].MembershipFunctions[1].type = "smf"
-    fis.input[0].MembershipFunctions[1].params = [-0.5, 0.5]
+    test()
+    # pass
+    # fis = fuzzy("Cartpole-controller", 1, 2, 1 ,2)
+    # fis.input[0].name = "Theta"
+    # fis.input[0].range = [-math.pi, math.pi]
+    # fis.input[0].MembershipFunctions[0].name = "Negative"
+    # fis.input[0].MembershipFunctions[0].type = "zmf"
+    # fis.input[0].MembershipFunctions[0].params = [-0.5, 0.5]
+    # fis.input[0].MembershipFunctions[1].name = "Positives"
+    # fis.input[0].MembershipFunctions[1].type = "smf"
+    # fis.input[0].MembershipFunctions[1].params = [-0.5, 0.5]
     
-    fis.output[0].name = "force"
-    fis.output[0].range = [-15, 15]
-    fis.output[0].MembershipFunctions[0].name = "NM"
-    fis.output[0].MembershipFunctions[0].type = "gbellmf"
-    fis.output[0].MembershipFunctions[0].params = [5, 2, -12]
-    fis.output[0].MembershipFunctions[1].name = "PM"
-    fis.output[0].MembershipFunctions[1].type = "gbellmf"
-    fis.output[0].MembershipFunctions[1].params = [5, 2, 12]
+    # fis.output[0].name = "force"
+    # fis.output[0].range = [-15, 15]
+    # fis.output[0].MembershipFunctions[0].name = "NM"
+    # fis.output[0].MembershipFunctions[0].type = "gbellmf"
+    # fis.output[0].MembershipFunctions[0].params = [5, 2, -12]
+    # fis.output[0].MembershipFunctions[1].name = "PM"
+    # fis.output[0].MembershipFunctions[1].type = "gbellmf"
+    # fis.output[0].MembershipFunctions[1].params = [5, 2, 12]
 
-    dt = 0.01
-    stop_theta = 50
-    theta = np.arange(-50, stop_theta + dt, dt)
+
+    # rules = []
+
+    # dt = 0.01
+    # stop_theta = 50
+    # theta = np.arange(-50, stop_theta + dt, dt)
     
 
-    smf_o = np.zeros(len(theta))
-    zmf_o = np.zeros(len(theta))
-    gbellmf_o1 = np.zeros(len(theta))
-    gbellmf_o2 = np.zeros(len(theta))
-    for i in range (0, len(theta)):
-        zmf_o[i] = fis.input[0].MembershipFunctions[0].getFuzzyValue(theta[i])
-        smf_o[i] = fis.input[0].MembershipFunctions[1].getFuzzyValue(theta[i])
-        gbellmf_o1[i] = fis.output[0].MembershipFunctions[0].getFuzzyValue(theta[i])
-        gbellmf_o2[i] = fis.output[0].MembershipFunctions[1].getFuzzyValue(theta[i])
+    # smf_o = np.zeros(len(theta))
+    # zmf_o = np.zeros(len(theta))
+    # gbellmf_o1 = np.zeros(len(theta))
+    # gbellmf_o2 = np.zeros(len(theta))
+    # for i in range (0, len(theta)):
+    #     zmf_o[i] = fis.input[0].MembershipFunctions[0].getFuzzyValue(theta[i])
+    #     smf_o[i] = fis.input[0].MembershipFunctions[1].getFuzzyValue(theta[i])
+    #     gbellmf_o1[i] = fis.output[0].MembershipFunctions[0].getFuzzyValue(theta[i])
+    #     gbellmf_o2[i] = fis.output[0].MembershipFunctions[1].getFuzzyValue(theta[i])
 
 
-    # plt.plot(theta, zmf_o, label='zmf', color='blue', linewidth=1)
-    # plt.plot(theta, smf_o, label='smf', color='green', linewidth=1)
-    plt.plot(theta, gbellmf_o1, label='gbell', color='green', linewidth=1)
-    plt.plot(theta, gbellmf_o2, label='gbell', color='red', linewidth=1)
+    # # plt.plot(theta, zmf_o, label='zmf', color='blue', linewidth=1)
+    # # plt.plot(theta, smf_o, label='smf', color='green', linewidth=1)
+    # plt.plot(theta, gbellmf_o1, label='gbell', color='green', linewidth=1)
+    # plt.plot(theta, gbellmf_o2, label='gbell', color='red', linewidth=1)
 
 
-    plt.xlabel('Theta')
-    plt.ylabel('Values')
-    plt.title('Plot of zmf and smf vs Theta')
-    plt.legend()
-    plt.grid(True)
-    plt.show()
+    # plt.xlabel('Theta')
+    # plt.ylabel('Values')
+    # plt.title('Plot of zmf and smf vs Theta')
+    # plt.legend()
+    # plt.grid(True)
+    # plt.show()
 
     
